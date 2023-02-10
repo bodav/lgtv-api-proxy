0005cd8c8856f8729a54246d9ea9fec5562373c0
bodav <bodavidsen@gmail.com>
2023-02-10 20:16:09 +0100

    converted to typescript

---
+++ src/app.ts
@@ -0,0 +1,62 @@
+import express from "express";
+import TvApi from "node-lgtv-api";
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const port = parseInt(process.env.PORT as string) || 3000;
+const tvip: string = process.env.TVIP ?? "192.168.1.207";
+const tvport = 8080;
+
+const cmds = Object.fromEntries(
+  Object.entries(TvApi).filter(([key, val]) => key.startsWith("TV_CMD_"))
+);
+
+app.get("/api/lgtv/pair", (req, res) => {
+  const pairApi = new TvApi(tvip, tvport);
+  pairApi.setDebugMode(true);
+  pairApi.displayPairingKey().then(
+    (value: any) => {
+      res.send({ resp: value });
+    },
+    (reason: Error) => {
+      console.error(reason);
+      res.status(500).send(reason);
+    }
+  );
+});
+
+app.get("/api/lgtv/:key/handlekeyinput/:cmd", (req, res) => {
+  const tvapi = new TvApi(tvip, tvport, req.params.key);
+  tvapi.setDebugMode(true);
+
+  tvapi.authenticate().then(
+    () => {
+      const cmd = cmds[req.params.cmd.toUpperCase()];
+      console.log(`Command ${req.params.cmd}. val ${cmd}`);
+
+      tvapi.processCommand("HandleKeyInput", { value: cmd }).then(
+        (value: any) => {
+          res.send({ resp: value });
+        },
+        (reason: Error) => {
+          console.error(reason);
+          res.status(500).send({ resp: reason.toString() });
+        }
+      );
+    },
+    (reason: Error) => {
+      console.error(reason);
+      res.status(500).send({ resp: reason.toString() });
+    }
+  );
+});
+
+app.get("/api/lgtv/commands", (req, res) => {
+  res.send(cmds);
+});
+
+app.listen(3000, () => {
+  console.log(`LG TV api proxy listening on port ${port}`);
+});
