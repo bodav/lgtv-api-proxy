6cabedcdfec63bd478794d86d03094b8a51664a2
bodav <bodavidsen@gmail.com>
2023-02-11 20:46:30 +0100

    async test

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import TvApi from "node-lgtv-api";
+import morgan from "morgan";
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(morgan("combined"));
 
 const port = parseInt(process.env.PORT as string) || 3000;
 const tvip: string = process.env.TVIP ?? "192.168.1.207";
@@ -13,18 +15,16 @@ const cmds = Object.fromEntries(
   Object.entries(TvApi).filter(([key, val]) => key.startsWith("TV_CMD_"))
 );
 
-app.get("/api/lgtv/pair", (req, res) => {
+app.get("/api/lgtv/pair", async (req, res) => {
   const pairApi = new TvApi(tvip, tvport);
   pairApi.setDebugMode(true);
-  pairApi.displayPairingKey().then(
-    (value: any) => {
-      res.send({ resp: value });
-    },
-    (reason: Error) => {
-      console.error(reason);
-      res.status(500).send(reason);
-    }
-  );
+
+  try {
+    const resp = await pairApi.displayPairingKey();
+    res.send({ result: resp });
+  } catch (error) {
+    res.status(500).send({ result: error });
+  }
 });
 
 app.get("/api/lgtv/:key/handlekeyinput/:cmd", (req, res) => {
